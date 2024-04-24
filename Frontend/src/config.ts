8e62e0b28899d4f96400f38f22dbadb17b8b0d02
JP5252 <44101227+JP5252@users.noreply.github.com>
2024-04-24 00:15:53 -0600

    Update config.ts

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-	apiURL: "http://localhost:8000",
+	apiURL: "https://314bfe59-010d-469f-b340-b1e20ced2dc7-dev.e1-us-cdp-2.choreoapis.dev/movie-review-app/backend/rest-api-be2/v1.0",
   };
